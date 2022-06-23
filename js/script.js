530060578f477d291b4367bb0da2b1c1c089b43c
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-23 15:22:06 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var app = new Vue({
     el: '#root',
     data: {
       message: 'Che bello iniziare a lavorare con i Framework!',
-      image: 'img/vue-logo.png'
+      image: 'vue-logo.png'
     }
   })
